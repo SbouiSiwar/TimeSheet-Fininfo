1b44cc34cc461d1eb2ccf1306b60af3198b300e0
ssboui <ssboui>
2022-07-19 09:38:26 +0100

    first commit

---
+++ src/main/java/com/fininfo/timesheet/TimesheetApplication.java
@@ -0,0 +1,13 @@
+package com.fininfo.timesheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimesheetApplication.class, args);
+	}
+
+}
